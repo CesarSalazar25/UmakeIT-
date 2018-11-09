7d6b82833732774e41fb8e6da10a7a74671d0284
Carlos De Castro <cadecastro23@gmail.com>
2018-11-08 23:17:25 -0400

    V 2.3 - Conexión con API de Paypal

--- src/app/components/carrito/carrito.component.ts
+++ src/app/components/carrito/carrito.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked} from '@angular/core';
 import { CartService } from '../../services/cart.service';
 import { AuthService } from '../../auth/auth.service';
 import { Cart } from '../../models/cart';
 import { Product } from '../../models/product';
 import { Router } from '@angular/router';
 
+declare let paypal: any;
+
 @Component({
   selector: 'app-carrito',
   templateUrl: './carrito.component.html',
   styleUrls: ['./carrito.component.css']
 })
-export class CarritoComponent implements OnInit {
+export class CarritoComponent implements OnInit, AfterViewChecked {
   
   cart: any;
   total: number = 0;
@@ -18,6 +20,8 @@ export class CarritoComponent implements OnInit {
   totalIva: number =0;
   User_id: string;
   extrasName: string;
+  paypalLoad: boolean = true;
+  addScript: boolean = false;
 
   constructor(
     private CartService: CartService,
@@ -54,4 +58,64 @@ export class CarritoComponent implements OnInit {
   clearCart(){
     this.CartService.resetCart(this.cart.id);
   }
+
+// Variable paypalConfig
+  paypalConfig = {
+    env: 'sandbox',
+
+    style: {
+      label: 'paypal',
+      size:  'medium',    // small | medium | large | responsive
+      shape: 'rect',     // pill | rect
+      color: 'blue',     // gold | blue | silver | black
+      tagline: false      
+  },
+    client: {
+        sandbox: 'AZC2Qq_ii09SCT7iRUTjEBrF4a8vx2nUi1ByXmLNT_A-iSsqOTQoyeX8AItdNDaDWiGhrsbwwlbx6nL1',
+        production: '<production-key>'
+    },
+    commit: true,
+    payment: (data, actions) => {
+        return actions.payment.create({
+            payment: {
+                transactions: [
+                    {
+                        amount: { total: this.totalIva , currency: 'USD' }
+                    }
+                ]
+            }
+        })
+    },
+  
+    // onAuthorize() is called when the buyer approves the payment
+    onAuthorize:(data, actions) => {
+  
+        // Make a call to the REST api to execute the payment
+        return actions.payment.execute().then((payment) => {
+            window.alert('Payment Complete!');
+        })
+    }
+  };
+  
+  ngAfterViewChecked(): void {
+    //Called after every check of the component's view. Applies to components only.
+    //Add 'implements AfterViewChecked' to the class.
+    if(!this.addScript) {
+      this.addPaypalScript().then(() => {
+        paypal.Button.render(this.paypalConfig, '#paypal-checkout-btn');
+        this.paypalLoad = true;
+      })
+    }
+  }
+
+  addPaypalScript(){
+    this.addScript = true;
+    return new Promise((resolve, reject) => {
+      let scriptElement = document.createElement('script');
+      scriptElement.src = 'https://www.paypalobjects.com/api/checkout.js'
+      scriptElement.onload = resolve;
+      document.body.appendChild(scriptElement);
+    })
+  }
 }
+  
